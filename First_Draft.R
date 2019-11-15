968250c92df0e0407082cfd360c080dea67fc5ca
katlynse <katlynse@buffalo.edu>
2019-11-14 21:20:17 -0500

    base_map

--- First_Draft.R
+++ First_Draft.R
@@ -1,5 +1,12 @@
-library(ncdf4)
-library(raster)
-library(plot3D)
+library(ncdf4) # package for netcdf manipulation
+library(raster) # package for raster manipulation
+library(rgdal) # package for geospatial analysis
+library(ggplot2) # package for plotting
+library(plot3D) #package for 3D plotting
 
-tn188_25_15 <- read.cdffile("")
+sm <- raster("dsa2_2m.grd")
+plot(sm)
+
+sm_crop <- crop(sm, extent(615000, 618000, 232000, 234000))
+
+plot(sm_crop)
