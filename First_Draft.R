f3bf5477d5f7f336f7cdc1d03f38fd6cfa9aa427
katlynse <katlynse@buffalo.edu>
2019-12-02 12:22:10 -0500

    Update First_Draft.R

--- First_Draft.R
+++ First_Draft.R
@@ -6,13 +6,27 @@ library(data.table)
 library(readxl) #read excel file
 library(sf)
 library(rasterVis)
+
 proj="+proj=utm +zone=15 +ellps=WGS84 +datum=WGS84 +units=m +no_defs"
 
 sm <- raster("data/empanada_clip.tif")*(-1)
+
 projection(sm)=proj
+
 sm_slope=terrain(sm,opt="slope",unit="degrees")
 
 sm_rough=terrain(sm,opt="roughness")
+
+sm_df=as.data.frame(sm)
+
+dive_4607 <- read.csv("data/sm_track.csv")
+
+dive_4607_sf <- read.csv("data/sm_track.csv")%>%
+  na.omit() %>%
+  st_as_sf(coords=c("Easting", "Northing")) %>%
+  st_set_crs("+proj=utm +zone=15 +ellps=WGS84 +datum=WGS84 +units=m +no_defs")
+
+
 
 sm_matrix <- as.matrix(sm) #don't need?
 
@@ -23,14 +37,7 @@ emp1 <- gplot(sm) +
 emp1 + geom_sf(data=dive_4607_sf, inherit.aes = F,col="red" )
   
  
-sm_df=as.data.frame(sm)
 
-dive_4607 <- read.csv("data/sm_track.csv")
-
-dive_4607_sf <- read.csv("data/sm_track.csv")%>%
-  na.omit() %>%
-  st_as_sf(coords=c("Easting", "Northing")) %>%
-  st_set_crs("+proj=utm +zone=15 +ellps=WGS84 +datum=WGS84 +units=m +no_defs")
  
 #roughness
 emp1 <- gplot(sm_rough>8,maxpixels=1e5) +
