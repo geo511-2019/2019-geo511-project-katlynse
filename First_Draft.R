fb0d7f0ec2676498c535a14722e906c1a2a2027d
katlynse <katlynse@buffalo.edu>
2019-12-03 13:23:37 -0500

    edits to rmd

--- First_Draft.R
+++ First_Draft.R
@@ -24,7 +24,7 @@ dive_4607 <- read.csv("data/sm_track.csv")
 dive_4607_sf <- read.csv("data/sm_track.csv")%>%
   na.omit() %>%
   st_as_sf(coords=c("Easting", "Northing")) %>%
-  st_set_crs("+proj=utm +zone=15 +ellps=GRS80 +datum=NAD83 +units=m +no_defs")
+  st_set_crs(proj)
 
 
 
@@ -45,7 +45,7 @@ emp2
 
  
 #emp3=sentry data with roughness
-emp3 <- gplot(sm_rough>3,maxpixels=1e5) +
+emp3 <- gplot(sm_rough,maxpixels=1e5) +
   geom_tile(aes(fill=value))+
   scale_fill_viridis_c()
  
@@ -55,7 +55,7 @@ emp4 <- emp3+dive_track
 emp4 
 
 #sentry with slope
-emp5 <- gplot(sm_slope>30,maxpixels=1e5) +
+emp5 <- gplot(sm_slope,maxpixels=1e5) +
   geom_tile(aes(fill=value))+
   scale_fill_viridis_c() 
 
