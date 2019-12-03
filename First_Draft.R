91e27ebfa13f6bd9d7c5518f460edc2892e5f45b
katlynse <katlynse@buffalo.edu>
2019-12-03 12:02:30 -0500

    editing rmd

--- First_Draft.R
+++ First_Draft.R
@@ -7,7 +7,7 @@ library(readxl) #read excel file
 library(sf)
 library(rasterVis)
 
-proj="+proj=utm +zone=15 +ellps=WGS84 +datum=WGS84 +units=m +no_defs"
+proj="+proj=utm +zone=15 +ellps=GRS80 +datum=NAD83 +units=m +no_defs"
 
 sm <- raster("data/empanada_clip.tif")*(-1)
 
@@ -24,28 +24,47 @@ dive_4607 <- read.csv("data/sm_track.csv")
 dive_4607_sf <- read.csv("data/sm_track.csv")%>%
   na.omit() %>%
   st_as_sf(coords=c("Easting", "Northing")) %>%
-  st_set_crs("+proj=utm +zone=15 +ellps=WGS84 +datum=WGS84 +units=m +no_defs")
+  st_set_crs("+proj=utm +zone=15 +ellps=GRS80 +datum=NAD83 +units=m +no_defs")
 
 
 
 sm_matrix <- as.matrix(sm) #don't need?
 
+dive_track <- geom_sf(data=dive_4607_sf, inherit.aes = F,col="red")
+#emp1=map of sentry data
 emp1 <- gplot(sm) +
   geom_tile(aes(fill=value))+
   scale_fill_viridis_c()
- 
-emp1 + geom_sf(data=dive_4607_sf, inherit.aes = F,col="red" )
+
+emp1 
+
+#emp2=dive track over sentry data
+emp2 <- emp1 + dive_track
   
- 
+emp2 
 
  
-#roughness
-emp1 <- gplot(sm_rough>8,maxpixels=1e5) +
+#emp3=sentry data with roughness
+emp3 <- gplot(sm_rough>3,maxpixels=1e5) +
   geom_tile(aes(fill=value))+
   scale_fill_viridis_c()
  
+#emp4=sentry with roughness and dive track
+emp4 <- emp3+dive_track
 
+emp4 
 
+#sentry with slope
+emp5 <- gplot(sm_slope>30,maxpixels=1e5) +
+  geom_tile(aes(fill=value))+
+  scale_fill_viridis_c() 
+
+emp5
+
+#emp6=sentry with slope and dive track
+emp6 <- emp5+dive_track
+
+emp6
 
 #just fancy looking don't use for analysis
 plot_ly(z= ~sm_matrix, x=xFromCol(sm), y=yFromRow(sm)) %>% 
